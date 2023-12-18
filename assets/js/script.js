6e2f655f72f4e695a76d708227de53e59f116c0a
JennyferValeskaVera18 <valevarillas13@gmail.com>
2023-12-18 18:10:49 -0500

    Incriptador con java

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+function cifrar() {
+    var mensaje = document.getElementById("mensaje").value;
+    var offset = parseInt(document.getElementById("offset").value);
+    var textoCifrado = "";
+
+    for (let c of mensaje) {
+        // Mayúsculas
+        if (c.charCodeAt(0) >= 65 && c.charCodeAt(0) <= 90) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 65;
+            codigo = (codigo + offset) % 26;
+            codigo += 65;
+            textoCifrado += String.fromCharCode(codigo);
+        }
+        // Minúsculas
+        else if (c.charCodeAt(0) >= 97 && c.charCodeAt(0) <= 122) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 97;
+            codigo = (codigo + offset) % 26;
+            codigo += 97;
+            textoCifrado += String.fromCharCode(codigo);
+        }
+        // Números
+        else if (!isNaN(parseInt(c))) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 48;
+            codigo = (codigo + offset) % 10;
+            codigo += 48;
+            textoCifrado += String.fromCharCode(codigo);
+        }
+        // Espacios 
+        else if ( c == "") {
+                textoCifrado += "";
+        }
+        
+        else {
+            textoCifrado += c;
+        }
+    }
+
+
+
+    document.getElementById("cifrado").value = textoCifrado;
+}
+
+function descifrar() {
+    var mensajeCifrado = document.getElementById("mensaje").value;
+    var offset = parseInt(document.getElementById("offset").value);
+    var textoDescifrado = "";
+
+    for (let c of mensajeCifrado) {
+        // Mayúsculas
+        if (c.charCodeAt(0) >= 65 && c.charCodeAt(0) <= 90) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 65;
+            codigo = (codigo - offset + 26) % 26;
+            codigo += 65;
+            textoDescifrado += String.fromCharCode(codigo);
+        }
+        // Minúsculas
+        else if (c.charCodeAt(0) >= 97 && c.charCodeAt(0) <= 122) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 97;
+            codigo = (codigo - offset + 26) % 26;
+            codigo += 97;
+            textoDescifrado += String.fromCharCode(codigo);
+        }
+        // Números
+        else if (!isNaN(parseInt(c))) {
+            let codigo = c.charCodeAt(0);
+            codigo -= 48;
+            codigo = (codigo - offset + 10) % 10;
+            codigo += 48;
+            textoDescifrado += String.fromCharCode(codigo);
+        }
+        // Espacios 
+        else if ( c == "") {
+            textoDescifrado += "";
+        }
+        //Otros caracteres
+        else {
+            textoDescifrado += c;
+        }
+    }
+
+    
+    document.getElementById("cifrado").value = textoDescifrado;
+}
+
